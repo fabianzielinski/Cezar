93cfdb0c11fd045c97b112f0e5bd2f4b4a1c8637
fabianzielinski <fabz@o2.pl>
2020-04-12 03:59:01 +0200

    Change encrypt function

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
 "use strict";
 
-var alph  = ['a','ą','b','c','ć','d','e','ę','f','g','h','i','j','k','l','ł','m','n','ń','o','ó','p','q','r','s','ś','t','u','v','w',
-'x','y','z','ź','ż', ' ']; 
+
+/* var alph = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w',
+	'x', 'y', 'z', 'ź', 'ż', ' ']; */
 
 var hendEncrypt = document.getElementById('bttnZaszyfruj');
 
 var hendDecrypt = document.getElementById('bttnOdszyfruj');
 
 function funcEncrypt() {
-		var nplus = document.getElementById('input').value;
-   		cezar(alph, nplus, 'en');
+	var nplus = document.getElementById('entextarea').value;
+	document.getElementById('detextarea').innerHTML = encryp(nplus);
+	console.log(encryp(nplus));
 }
 
 function funcDecrypt() {
-			var nplus = Number(alph.length) - Number(alph.indexOF(document.getElementsByTagName('input')));
-   			cezar(alph, nplus, 'de');
+	let inputText = document.getElementById('input');
+	var nplus = Number(alph.length) - Number(alph.indexOF(inputText));
+	cezar(alph, nplus, 'de');
 }
 
-function cezar(alph, nplus, direction) {
- 				var Ta = document.getElementById(direction +"textarea").value;
-           		Text = Ta.split("");  
-     		    var Coded = [];
-        		Text.forEach(function (sVal, j) {
-       				var poz_w_alph = alph.indexOF(sVal);
-               		if (poz_w_alph != -1) {
-               	 		var new_poz = Number(nplus) + poz_w_alph;
-               	 		if (new_poz >= alph.length) {new_poz = new_poz - alph.length}
- 								Coded.push(alph[new_poz]);
-               }        
-        });
-        var req_stmt = Coded.join("");
-        $("#"+ direction +"crypt").html(req_stmt);  
- }
+function encryp(str) {
+	return str.split('').map((char) => {
+		var x = char.charCodeAt(0);
+		if (x < 65 || x > 90) {
+			return String.fromCharCode(x);
+		} else if (x < 78) {
+			return String.fromCharCode(x + 13);
+		} else {
+			return String.fromCharCode(x - 13);
+		}
+
+	}).join('');
+
+}
 
 hendEncrypt.addEventListener("click", funcEncrypt);
-				
+
 
 hendDecrypt.addEventListener('click', funcDecrypt);
