b3f86b7f02e2d43325a8da267cc9293272140486
fabianzielinski <fabian.zielinski@o2.pl>
2020-04-25 20:35:20 +0200

    New ver

---
+++ dist/js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+/* var alph = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w',
+	'x', 'y', 'z', 'ź', 'ż', ' ']; */
+
+var hendEncrypt = document.getElementById('bttnZaszyfruj');
+
+var hendDecrypt = document.getElementById('bttnOdszyfruj');
+
+
+
+function funcEncrypt() {
+	var nplus = document.getElementById('entextarea').value;
+	var keyValue = document.getElementById('inputNumber').value;
+	document.getElementById('detextarea').innerHTML = encryp(nplus, keyValue);
+}
+
+function funcDecrypt() {
+	let inputText = document.getElementById('input');
+	var nplus = Number(alph.length) - Number(alph.indexOF(inputText));
+	cezar(alph, nplus, 'de');
+}
+
+function encryp(str, keyValue) {
+	return str.split('').map((char) => {
+		var x = char.charCodeAt(0);
+		if (x > 64 && x < 91) {
+			console.log("A");
+			console.log(((x - 65 + keyValue) % 26) + 65);
+			return String.fromCharCode(((x - 65 + keyValue) % 26) + 65);
+		} else if (x > 96 && x < 123) {
+			console.log("a");
+			console.log((((x - 97 + keyValue) % 26) + 97));
+			let namber = x - 97 + parseInt(keyValue);
+			console.log('x: ' + x);
+			console.log('keyValue: ' + keyValue);
+			console.log('namber: ' + namber);
+			console.log('mod: ' + (namber % 26));
+			return String.fromCharCode((((x - 97 + keyValue) % 26) + 97));
+		} else {
+			console.log('#');
+			return String.fromCharCode(x);
+		}
+
+	}).join('');
+
+}
+
+hendEncrypt.addEventListener("click", funcEncrypt);
+
+
+hendDecrypt.addEventListener('click', funcDecrypt);
