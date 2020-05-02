989abb31731801c31a74b69254cc59bbecc6e6a2
fabianzielinski <fabian.zielinski@o2.pl>
2020-05-02 22:22:34 +0200

    Finish

--- dist/bundle.js
+++ dist/bundle.js
@@ -101,20 +101,20 @@ function encryp(str, keyValue) {
 	return str.split('').map(function (char) {
 		var x = char.charCodeAt(0);
 		if (x > 64 && x < 91) {
-			console.log("AA");
-			console.log((x - 65 + parseInt(keyValue)) % 26 + 65);
+			// console.log("AA");
+			// console.log(((x - 65 + parseInt(keyValue)) % 26) + 65);
 			return String.fromCharCode((x - 65 + parseInt(keyValue)) % 26 + 65);
 		} else if (x > 96 && x < 123) {
-			console.log("aa");
-			console.log((x - 97 + parseInt(keyValue)) % 26 + 97);
-			var namber = x - 97 + parseInt(keyValue);
-			console.log('x: ' + x);
-			console.log('keyValue: ' + parseInt(keyValue));
-			console.log('namber: ' + namber);
-			console.log('mod: ' + namber % 26);
+			// console.log("aa");
+			// console.log((((x - 97 + parseInt(keyValue)) % 26) + 97));
+			// let namber = x - 97 + parseInt(keyValue);
+			// console.log('x: ' + x);
+			// console.log('keyValue: ' + parseInt(keyValue));
+			// console.log('namber: ' + namber);
+			// console.log('mod: ' + (namber % 26));
 			return String.fromCharCode((x - 97 + parseInt(keyValue)) % 26 + 97);
 		} else {
-			console.log('#');
+			// console.log('#');
 			return String.fromCharCode(x);
 		}
 	}).join('');
@@ -137,7 +137,7 @@ function decryp(str, keyValue) {
 			// console.log('mod: ' + (namber % 26));
 			return String.fromCharCode(122 - (122 - x + parseInt(keyValue)) % 26);
 		} else {
-			console.log('#');
+			// console.log('#');
 			return String.fromCharCode(x);
 		}
 	}).join('');
